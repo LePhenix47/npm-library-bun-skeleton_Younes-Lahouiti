772c68e6e58107de48c6ca980bb9de92d21ddcf6
Younes Lahouiti <loller763@gmail.com>
2024-04-06 10:49:13 +0200

    First commit to initialize the template

---
+++ bun.build.ts
@@ -0,0 +1,29 @@
+import dts from "bun-plugin-dts-auto";
+
+console.log("Building...");
+
+const result = await Bun.build({
+  entrypoints: ["src/index.ts"],
+  outdir: "dist",
+  target: "bun",
+
+  plugins: [
+    dts({
+      cwd: import.meta.dir,
+    }),
+  ],
+});
+
+if (!result.success) {
+  console.error("Build failed");
+
+  for (const message of result.logs) {
+    // Bun will pretty print the message object
+    console.error(message);
+  }
+
+  process.exit(1);
+}
+
+// eslint-disable-next-line no-console
+console.log("Build complete");
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+type Test = "test" | "not-test";
+const a: Test = "test";
+console.log(a, 3);
